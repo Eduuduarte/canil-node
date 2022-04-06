8bb0636edf33ecb735a20f0d7808d596aed07bb9
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-04-06 19:43:47 -0300

    Projeto final 1.0

--- src/serve.ts
+++ src/serve.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
-import mainRoutes from './Routes/index';
+import mainRoutes from './routes/index';
 
 dotenv.config();
 
@@ -18,7 +18,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req, res)=>{
-    res.send("Página não encontrada!");
+    res.render('pages/404');
 });
 
 
